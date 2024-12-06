85cd58dbdbb08003ca57646b03f577643435055b
chathamabate <chathamabate@gmail.com>
2024-12-05 20:16:46 -0700

    Serial type creation

--- chrpc/include/chrpc/serial.h
+++ chrpc/include/chrpc/serial.h
@@ -3,39 +3,17 @@
 #define CHRPC_SERIAL_H
 
 #include <stdlib.h>
-
-// Tags will be encoded as single bytes.
-typedef enum _chrpc_serial_tag_t {
-    // The _T suffix signifies a tag used with type annotation.
-    
-    // Primitive Types.
-    CHRPC_BYTE_T_TAG = 0,
-
-    // Numeric Types are expected to be serialized in little endian.
-    CHRPC_INT16_T_TAG,
-    CHRPC_INT32_T_TAG,
-    CHRPC_INT64_T_TAG,
-
-    CHRPC_UINT16_T_TAG,
-    CHRPC_UINT32_T_TAG,
-    CHRPC_UINT64_T_TAG,
-
-    // Null terminated array of characters.
-    CHRPC_STRING_T_TAG, 
-
-    // Composite Types.
-    CHRPC_STRUCT_T_START_TAG,
-    CHRPC_STRUCT_T_END_TAG,
-
-    CHRPC_ARRAY_T_TAG,
-} chrpc_serial_tag_t;
+#include <stdbool.h>
 
 // Type Grammar:
 // PRIM ::= BYTE | INT | UINT | STRING
-// COMP ::= STRUCT_START TYPE+ STRUCT_END 
+// COMP ::= STRUCT <NUM_FIELDS> TYPE+ 
 //          | ARRAY TYPE
 // TYPE ::= COMP | PRIM
-
+//
+// The number of fields a struct type has will be encoded as a single
+// unsigned byte. THUS, a struct can have a maximum of 255 fields.
+//
 // Values will parsed after parsing the prepended type signature.
 // The only values which are serialized in a somewhat unique way are
 // strings, which end with \0, and arrays which start with a 32-bit unsigned 
@@ -43,25 +21,39 @@ typedef enum _chrpc_serial_tag_t {
 //
 // NOTE: ALL MULTI BYTE NUMBERIC VALUES ARE LITTLE ENDIAN!
 
+// A collection of status codes used across this library.
+typedef enum _chrpc_status_t {
+    CHRPC_SUCCESS = 0,
+    CHRPC_SYNTAX_ERROR,
 
-// These IDs are used internally to this library and are NEVER sent over
-// serial.
+    // Returned when the buffer ends before a parse is complete.
+    CHRPC_UNEXPECTED_END,
+
+    // Returned if the given buffer isn't large enough.
+    CHRPC_BUFFER_TOO_SMALL,
+} chrpc_status_t;
+
 typedef enum _chrpc_type_id_t {
-    CHRPC_BYTE_T = 0,
+    CHRPC_BYTE_TID = 0,
 
-    CHRPC_INT16_T,
-    CHRPC_INT32_T,
-    CHRPC_INT64_T,
+    CHRPC_INT16_TID,
+    CHRPC_INT32_TID,
+    CHRPC_INT64_TID,
 
-    CHRPC_UINT16_T,
-    CHRPC_UINT32_T,
-    CHRPC_UINT64_T,
+    CHRPC_UINT16_TID,
+    CHRPC_UINT32_TID,
+    CHRPC_UINT64_TID,
 
-    CHRPC_STRING_T, 
+    CHRPC_STRING_TID, 
 
-    CHRPC_STRUCT_T,
-    CHRPC_ARRAY_T,
+    CHRPC_STRUCT_TID,
+    CHRPC_ARRAY_TID,
 } chrpc_type_id_t;
+
+// This relies on primite type IDs being defined consecutively.
+static inline bool chrpc_type_id_is_primitive(chrpc_type_id_t tid) {
+    return (CHRPC_BYTE_TID <= tid && tid <= CHRPC_STRING_TID);
+}
 
 // A Tree like structure for helping specify and work with serializable types.
 
@@ -88,7 +80,41 @@ struct _chrpc_struct_fields_types_t {
     chrpc_type_t **field_types;
 };
 
+static inline bool chrpc_type_is_primitive(chrpc_type_t *ct) {
+    return chrpc_type_id_is_primitive(ct->type_id);
+}
+
+// Primite types will all be singletons stored in static memory.
+// You CAN call the type destructor on these types without error,
+// the destructor will simply do nothing.
+extern chrpc_type_t * const CHRPC_BYTE_T;
+extern chrpc_type_t * const CHRPC_INT16_T;
+extern chrpc_type_t * const CHRPC_INT32_T;
+extern chrpc_type_t * const CHRPC_INT64_T;
+extern chrpc_type_t * const CHRPC_UINT16_T;
+extern chrpc_type_t * const CHRPC_UINT32_T;
+extern chrpc_type_t * const CHRPC_UINT64_T;
+extern chrpc_type_t * const CHRPC_STRING_T; 
 
 
+// NOTE: Just like with the JSON library, these args for the composite types
+// are owned by the returned type.
+//
+// Deleting the returned type will delete the given internal type.
+chrpc_type_t *new_chrpc_array_type(chrpc_type_t *array_cell_type);
+
+// This expects a NULL terminated sequence of chrpc_type_t *'s.
+// dummy is not used, just for varargs.
+chrpc_type_t *_new_chrpc_struct_type(int dummy,...);
+
+// The given varargs cannot be empty.
+#define new_chrpc_struct_type(...) \
+    _new_chrpc_struct_type(0, __VA_ARGS__, NULL)
+
+
+void delete_chrpc_type(chrpc_type_t *ct);
+
+chrpc_status_t chrpc_type_to_buffer(chrpc_type_t *ct, uint8_t *buf, size_t buf_len, size_t *written);
+chrpc_status_t chrpc_type_from_buffer(uint8_t *buf, size_t buf_len, chrpc_type_t **ct, size_t *readden);
 
 #endif
---
+++ chrpc/src/serial.c
@@ -0,0 +1,116 @@
+
+#include "chrpc/serial.h"
+#include "chsys/mem.h"
+#include "chutil/list.h"
+#include <stdarg.h>
+
+static chrpc_type_t _CHRPC_BYTE_T = {
+    .type_id = CHRPC_BYTE_TID
+};
+static chrpc_type_t _CHRPC_INT16_T = {
+    .type_id = CHRPC_INT16_TID
+};
+static chrpc_type_t _CHRPC_INT32_T = {
+    .type_id = CHRPC_INT32_TID
+};
+static chrpc_type_t _CHRPC_INT64_T = {
+    .type_id = CHRPC_INT64_TID
+};
+static chrpc_type_t _CHRPC_UINT16_T = {
+    .type_id = CHRPC_UINT16_TID
+};
+static chrpc_type_t _CHRPC_UINT32_T = {
+    .type_id = CHRPC_UINT32_TID
+};
+static chrpc_type_t _CHRPC_UINT64_T = {
+    .type_id = CHRPC_UINT64_TID
+};
+static chrpc_type_t _CHRPC_STRING_T = {
+    .type_id = CHRPC_STRING_TID
+}; 
+
+chrpc_type_t * const CHRPC_BYTE_T = &_CHRPC_BYTE_T;
+chrpc_type_t * const CHRPC_INT16_T = &_CHRPC_INT16_T;
+chrpc_type_t * const CHRPC_INT32_T = &_CHRPC_INT32_T;
+chrpc_type_t * const CHRPC_INT64_T = &_CHRPC_INT64_T;
+chrpc_type_t * const CHRPC_UINT16_T = &_CHRPC_UINT16_T;
+chrpc_type_t * const CHRPC_UINT32_T = &_CHRPC_UINT32_T;
+chrpc_type_t * const CHRPC_UINT64_T = &_CHRPC_UINT64_T;
+chrpc_type_t * const CHRPC_STRING_T = &_CHRPC_STRING_T; 
+
+chrpc_type_t *new_chrpc_array_type(chrpc_type_t *array_cell_type) {
+    chrpc_type_t *ct = (chrpc_type_t *)safe_malloc(sizeof(chrpc_type_t));
+    ct->type_id = CHRPC_ARRAY_TID;
+    ct->array_cell_type = array_cell_type;
+
+    return ct;
+}
+
+// Again, the given array will be placed into the created struct type. (i.e. ownership transfer)
+static chrpc_type_t *new_chrpc_struct_type_from_internals(size_t num_fields, chrpc_type_t **field_types) {
+    chrpc_struct_fields_types_t *sfts = (chrpc_struct_fields_types_t *)
+        safe_malloc(sizeof(chrpc_struct_fields_types_t));
+
+    sfts->num_fields = num_fields;
+    sfts->field_types = field_types;
+
+    chrpc_type_t *ct = (chrpc_type_t *)safe_malloc(sizeof(chrpc_type_t));
+    ct->type_id = CHRPC_STRUCT_TID;
+    ct->struct_fields_types = sfts;
+
+    return ct;
+}
+
+chrpc_type_t *_new_chrpc_struct_type(int dummy,...) {
+    va_list args;
+    va_start(args, dummy);
+
+    // Kinda awkward here, we will populate this array list,
+    // then afterwards copy it into a normal array.
+
+    chrpc_type_t *iter;
+    list_t *l = new_list(ARRAY_LIST_IMPL, sizeof(chrpc_type_t *));
+
+    while ((iter = va_arg(args, chrpc_type_t *))) {
+        l_push(l, &iter);
+    }
+
+    size_t num_fields = l_len(l);
+    chrpc_type_t **sfts = (chrpc_type_t **)
+        safe_malloc(sizeof(chrpc_type_t *) * num_fields);
+
+    for (size_t i = 0; i < num_fields; i++) {
+        l_get_copy(l, i, &(sfts[i]));
+    }
+
+    delete_list(l);
+
+    return new_chrpc_struct_type_from_internals(num_fields, sfts);
+}
+
+void delete_chrpc_type(chrpc_type_t *ct) {
+    if (chrpc_type_is_primitive(ct)) {
+        return; // DO NOTHING FOR SINGLETONS.
+    }
+
+    if (ct->type_id == CHRPC_ARRAY_TID) {
+        delete_chrpc_type(ct->array_cell_type);
+    } else if (ct->type_id == CHRPC_STRUCT_TID) {
+        for (size_t i = 0; i < ct->struct_fields_types->num_fields; i++) {
+            delete_chrpc_type(ct->struct_fields_types->field_types[i]);
+        }
+
+        safe_free(ct->struct_fields_types);
+    }
+
+    safe_free(ct);
+}
+
+chrpc_status_t chrpc_type_to_buffer(chrpc_type_t *ct, uint8_t *buf, size_t buf_len, size_t *written) {
+    return CHRPC_SUCCESS;
+}
+
+chrpc_status_t chrpc_type_from_buffer(uint8_t *buf, size_t buf_len, chrpc_type_t **ct, size_t *readden) {
+    return CHRPC_SUCCESS;
+}
+
