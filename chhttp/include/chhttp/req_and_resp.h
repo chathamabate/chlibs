09cd65a627709aa633839244da8b208d7c482db5
chathamabate <chathamabate@gmail.com>
2025-02-14 16:28:51 -0700

    Ok, we are getting somewhere, slowly

--- chhttp/include/chhttp/req_and_resp.h
+++ chhttp/include/chhttp/req_and_resp.h
@@ -6,14 +6,19 @@
 #include "chutil/string.h"
 #include "chutil/map.h"
 
-// Helper for creating a non-empty set of headers.
-// Returns NULL on error. This errors out when trying to set an invalid header.
-// For example, content length will be determined and written when necessary,
-// you cannot specify it manually here.
-//
-// Call these functions by alternating keys and values as C-strings/literals.
-hash_map_t *new_header_set_from_kvps(int dummy,...);
-#define new_header_set_from_kvps(...) 
+// A header map is really just a map<string *, string *>
+typedef hash_map_t header_map_t;
+
+static inline header_map_t *new_header_map() {
+    return new_hash_map(sizeof(string_t *), sizeof(string_t *), (hash_map_hash_ft)s_indirect_hash, (hash_map_key_eq_ft)s_indirect_equals);
+}
+
+void delete_header_map(header_map_t *hm);
+
+void header_map_put(const char *k, const char *v);
+
+// NOTE: Right now, we will not support sending chunked responses.
+// We will be able to receive them, but that's it.
 
 typedef enum _http_method_t {
     HTTP_GET = 0,
@@ -36,17 +41,26 @@ typedef struct _http_req_t {
     // NOTE: We don't store protocol as we will only support 
     // HTTP/1.1
 
-    // Maps<string_t *, string_t *>
-    hash_map_t headers; 
+    header_map_t *headers; 
 
     // NOTE: We don't support streamed requests.
     size_t body_length;
     void *body;
 } http_req_t;
 
+// The http request will take OWNERSHIP of the headers map, and the body.
+// The http request DOES NOT take OWNERSHIP of the target string, it will be copied.
+http_req_t *new_http_request_verbose(http_method_t m, const char *target, header_map_t *headers, void *body, size_t body_length);
 
+static inline http_req_t *new_http_request_bodied(http_method_t m, const char *target, void *body, size_t body_length) {
+    return new_http_request_verbose(m, target, new_header_map(), body, body_length);
+}
 
+static inline http_req_t *new_http_request(http_method_t m, const char *target) {
+    return new_http_request_bodied(m, target, NULL, 0);
+}
 
+void delete_http_request(http_req_t *req);
 
 
 #endif
--- chhttp/src/req_and_resp.c
+++ chhttp/src/req_and_resp.c
@@ -1,6 +1,24 @@
 
 #include <stdlib.h>
 #include "chhttp/req_and_resp.h"
+#include "chsys/mem.h"
+#include "chutil/map.h"
+#include "chutil/string.h"
+
+void delete_header_map(header_map_t *hm) {
+    key_val_pair_t kvp;
+    hm_reset_iterator(hm);
+    while ((kvp = hm_next_kvp(hm)) != HASH_MAP_EXHAUSTED) {
+        string_t **s;
+
+        s = (string_t **)kvp_key(hm, kvp);
+        delete_string(*s);
+
+        s = (string_t **)kvp_val(hm, kvp);
+        delete_string(*s);
+    }
+    delete_hash_map(hm);
+}
 
 const char *http_method_as_literal(http_method_t m) {
     switch (m) {
@@ -25,4 +43,28 @@ const char *http_method_as_literal(http_method_t m) {
         default:
             return NULL;
     }
+}
+
+http_req_t *new_http_request_verbose(http_method_t m, const char *target, hash_map_t *headers, void *body, size_t body_length) {
+    http_req_t *req = safe_malloc(sizeof(http_req_t)); 
+    
+    req->method = m;
+    req->target = new_string_from_cstr(target);
+    req->headers = headers;
+
+    req->body_length = body_length;
+    req->body = body;
+
+    return req;
+}
+
+void delete_http_request(http_req_t *req) {
+    delete_string(req->target);
+    delete_header_map(req->headers);
+
+    if (req->body) {
+        safe_free(req->body);
+    }
+
+    safe_free(req);
 }
